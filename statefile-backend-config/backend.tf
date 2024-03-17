15874dcf170d54c2db2ccc380c7b76d6290b6c46
kikiran <kiran.m79955.com>
2024-03-17 16:48:31 +0530

    applying lock with dynamodb table

--- statefile-backend-config/backend.tf
+++ statefile-backend-config/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "terraform-20240314063729088900000001"
     key    = "custinstance"
     region = "us-east-1"
+    dynamodb_table = "aws_dynamodb_table_state_lock"
+    encrypt = true
    
   }
 }

e3a9f26b66ca04807b1bb089f49b69b79de2a7b2
kikiran <kiran.m79955.com>
2024-03-21 17:28:17 +0530

    creating module for creation of Ec2 Instance

---
+++ terraform-module/variables.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+  description = "passing ami_id to main.tf"
+  type = string
+  default = ""
+}
+variable "instance_type" {
+  description = "describe ec2 type"
+  type = string
+  default = ""
+}
+variable "key_name" {
+  description = "passing keypair to main.tf"
+  type = string
+  default = ""
+}
+variable "tags" {
+  description = "passing tag to main.tf"
+  type = string
+  default = ""
+  
+}

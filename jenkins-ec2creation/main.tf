7e8e0612ac65f7b41153c4aa91309785397e9db6
kikiran <kiran.m79955.com>
2024-03-29 20:52:59 +0530

    ami id added

--- jenkins-ec2creation/main.tf
+++ jenkins-ec2creation/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "jenkins-instance" {
-    ami = "jenkins-instance"
+    ami = "ami-0c101f26f147fa7fd"
     instance_type = "t2.micro"
     key_name = "custinstance"
     tags = {

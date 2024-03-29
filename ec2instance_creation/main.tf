a683ef0542624c6f272efb95d6acbdc9a1924da9
Imandi KiranKumar <59505949+kikiran@users.noreply.github.com>
2024-03-29 12:30:50 +0530

    Update main.tf

--- ec2instance_creation/main.tf
+++ ec2instance_creation/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "localname" {
     key_name = var.key_name
     availability_zone = "us-east-1a"
     tags = {
-      Name: "dev"
+      Name: "dev-jenkins-creation"
     }
   
 }

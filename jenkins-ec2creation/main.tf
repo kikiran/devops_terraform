1aa08ef63ee64a43e3ac4f2082cfc8e2a114419d
Imandi KiranKumar <59505949+kikiran@users.noreply.github.com>
2024-03-29 21:13:04 +0530

    Update main.tf

--- jenkins-ec2creation/main.tf
+++ jenkins-ec2creation/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jenkins-instance" {
     ami = "ami-0c101f26f147fa7fd"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = "custinstance"
     tags = {
       Name: "Jenkins instance"

718c0c7edc669b44d8260bef0b94d519a1a1ab10
kikiran <kiran.m79955.com>
2024-04-10 18:35:30 +0530

    Attaching policy to the user group and add admin access

--- IAM_Create_attach_policy/main.tf
+++ IAM_Create_attach_policy/main.tf
@@ -1,4 +1,22 @@
+//Create a new user and tag with Devops
 resource "aws_iam_user" "create_iam_user" {
     name = "Cloud_Admin"
+    tags = {
+      Name: "DevOps"
+    }
   
+}
+
+//Attaching the admin user to the admin group
+resource "aws_iam_group_membership" "team" {
+  name = "admin"
+  users = [ aws_iam_user.create_iam_user.name ]
+  group = "admin"
+
+}
+//Attaching Administrator access policies to the user
+
+resource "aws_iam_group_policy_attachment" "adminaccess" {
+  group= "admin"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }

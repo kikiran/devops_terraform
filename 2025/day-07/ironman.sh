d2bb55993c4e2062682b073e205c73c9d78cf51d
imandi.kumar <imandi.kumar@upbound.com>
2025-09-16 20:08:28 +0530

    create iac code with terraform

---
+++ 2025/day-07/ironman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo su -
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Deployed via Terraform</h1>" > /var/www/html/index.html
+echo "<h2>IRON MAN was here</h2>" >> /var/www/html/index.html
+echo "<h3>Using AMI ID: ${ami_id}</h3>" >> /var/www/html/index.html
---
+++ 2025/day-07/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id_batman = "ami-0b09ffb6d8b58ca91"
+ami_id_ironman = "ami-0d85d4f07a62e2969"
